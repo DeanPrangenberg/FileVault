3e44aebdfe75063291d882649267ded84145b8e7
DP <prangenbergdean@gmail.com>
2024-12-16 06:31:08 +0100

    Delete RustFileCopy/target directory

--- RustFileCopy/target/debug/deps/RustFileCopy-bb7c97ce86a25e1c.d
+++
@@ -1,5 +0,0 @@
-D:\MyRepo\FileVault\RustFileCopy\target\debug\deps\libRustFileCopy-bb7c97ce86a25e1c.rmeta: src/main.rs
-
-D:\MyRepo\FileVault\RustFileCopy\target\debug\deps\RustFileCopy-bb7c97ce86a25e1c.d: src/main.rs
-
-src/main.rs:
--- RustFileCopy/target/release/deps/RustFileCopy.d
+++
@@ -1,5 +0,0 @@
-D:\MyRepo\FileVault\RustFileCopy\target\release\deps\RustFileCopy.exe: src/main.rs
-
-D:\MyRepo\FileVault\RustFileCopy\target\release\deps\RustFileCopy.d: src/main.rs
-
-src/main.rs:
